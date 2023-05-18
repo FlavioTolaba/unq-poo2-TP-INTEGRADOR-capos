79c2180b8084d4b7ade84fa18dfb8e73678f5ec2
AlejoVillaizan <alejofvillaizan@gmail.com>
2023-05-18 19:50:38 -0300

    Se hace Merge

---
+++ TP-INTEGRADOR/src/tpIntegrador/LosMasVinchucas.java
@@ -0,0 +1,21 @@
+package tpIntegrador;
+
+import java.util.List;
+
+public class LosMasVinchucas {
+	private List<Participante> participantes;
+	private List<Muestra> muestras;
+	private List<Organizacion> organizaciones;
+	
+public void participanteCargaMuestra(Participante participante){
+		//filtrar participante de la lista de participantes de la app
+		Muestra muestra=new Muestra();
+		participante.cargarMuestra();
+		this.participantes.add(participante);
+		}
+
+		public void crearParticipante(String id,Rango rango,boolean estudios){
+		Participante participante=new Participante(id,rango,estudios);
+		this.participantes.add(participante);
+		}
+}
---
+++ TP-INTEGRADOR/src/tpIntegrador/Ubicacion.java
@@ -0,0 +1,26 @@
+package tpIntegrador;
+
+public class Ubicacion {
+	private double longitudX;
+	private double latitudY;
+	
+	
+	public Ubicacion(double longitudX, double latitudY) {
+		this.longitudX = longitudX;
+		this.latitudY = latitudY;
+	}
+
+
+	public double getLongitudX() {
+		return this.longitudX;
+	}
+
+
+	public double getLatitudY() {
+		return this.latitudY;
+	}
+	
+
+
+
+}
